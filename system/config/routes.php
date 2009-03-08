658eac9e6641e2ce067faa0c088b4ae5c5a3a0f1
Woody Gilk <woody.gilk@kohanaphp.com>
2009-03-08 23:55:13 +0000

    Cleaned up missing end-of-file newlines and added SYSPATH checks to empty extension files

--- system/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array
-(
-	'default' =>
-		Route::factory('(<controller>(/<method>(/<id>)))')
-			->defaults(array('controller' => 'welcome', 'method' => 'index')),
-);
--- system/extensions/router.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-class Router extends Router_Core {}
