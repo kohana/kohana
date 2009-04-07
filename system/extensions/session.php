34276f14441eefeede135e19c8fbca639295a7ce
Woody Gilk <woody.gilk@kohanaphp.com>
2009-04-07 10:34:18 -0500

    Added session config file and empty session class extensions

---
+++ system/extensions/session.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Session extends Session_Core {}
---
+++ system/extensions/session/native.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Session_Native extends Session_Native_Core {}
