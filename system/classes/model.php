f91ed1a1363b2798278596ac75feecd38fbc1697
Woody Gilk <woody.gilk@kohanaphp.com>
2009-04-07 10:37:48 -0500

    Added abstract Model class and empty extension

---
+++ system/classes/model.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Model base class.
+ *
+ * @package    Kohana
+ * @author     Kohana Team
+ * @copyright  (c) 2008-2009 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+abstract class Model_Core {
+
+	protected $_db = 'default';
+
+	/**
+	 * Loads the database.
+	 * 
+	 * @return  void
+	 */
+	public function __construct()
+	{
+		if (is_string($this->_db))
+		{
+			// Load the database
+			$this->_db = Database::instance($this->_db)
+		}
+	}
+
+} // End Model
---
+++ system/extensions/model.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Model extends Model_Core {}
