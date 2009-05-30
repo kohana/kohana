0a517c909daafbb4f1edd2cdbbd284c0b3813b37
Woody Gilk <woody.gilk@kohanaphp.com>
2009-05-30 16:08:03 -0500

    Removed tests from application

--- application/views/test/index.php
+++
@@ -1,7 +0,0 @@
-<h1><?php echo ucwords($controller) ?> Tests</h1>
-
-<ul>
-<?php foreach ($tests as $url => $method): ?>
-	<li><a href="<?php echo $url ?>"><?php echo $method ?></a></li>
-<?php endforeach ?>
-</ul>
--- application/views/test/request/subreq.php
+++
@@ -1,6 +0,0 @@
-<h1>Sub-Requests Text</h1>
-
-<?php foreach ($cases as $uri => $response): ?>
-<p>Requested <code><?php echo $uri ?></code></p>
-<blockquote><?php echo $response ?></blockquote>
-<?php endforeach ?>
