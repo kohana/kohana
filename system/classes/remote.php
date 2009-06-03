6fd690911cbd1be173ab2005673e7a6b8567fafd
Woody Gilk <woody.gilk@kohanaphp.com>
2009-06-03 17:38:15 -0500

    Added remote::status() from 2.4, finishes remote helper porting

--- system/classes/remote.php
+++ system/classes/remote.php
@@ -59,4 +59,61 @@ class remote_Core {
 		return $response;
 	}
 
+	/**
+	 * Returns the status code for a URL.
+	 *
+	 * @param   string  URL to check
+	 * @return  integer
+	 */
+	public static function status($url)
+	{
+		// Get the hostname and path
+		$url = parse_url($url);
+
+		if (empty($url['path']))
+		{
+			// Request the root document
+			$url['path'] = '/';
+		}
+
+		// Open a remote connection
+		$remote = fsockopen($url['host'], 80, $errno, $errstr, 5);
+
+		if ( ! is_resource($remote))
+			return FALSE;
+
+		// Set CRLF
+		$CRLF = "\r\n";
+
+		// Send request
+		fwrite($remote, 'HEAD '.$url['path'].' HTTP/1.0'.$CRLF);
+		fwrite($remote, 'Host: '.$url['host'].$CRLF);
+		fwrite($remote, 'Connection: close'.$CRLF);
+		fwrite($remote, 'User-Agent: Kohana Framework (+http://kohanaphp.com/)'.$CRLF);
+
+		// Send one more CRLF to terminate the headers
+		fwrite($remote, $CRLF);
+
+		// Remote is offline
+		$response = FALSE;
+
+		while ( ! feof($remote))
+		{
+			// Get the line
+			$line = trim(fgets($remote, 512));
+
+			if ($line !== '' AND preg_match('#^HTTP/1\.[01] (\d{3})#', $line, $matches))
+			{
+				// Response code found
+				$response = (int) $matches[1];
+				break;
+			}
+		}
+
+		// Close the connection
+		fclose($remote);
+
+		return $response;
+	}
+
 } // End remote
