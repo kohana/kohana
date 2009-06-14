ab60177ac57243a9b9ab1d6fb20878160757c5a4
Woody Gilk <woody.gilk@kohanaphp.com>
2009-06-14 09:04:50 -0500

    Preparing bootstrap for beta release

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -4,7 +4,7 @@
 
 /**
  * Set the default time zone.
- * 
+ *
  * @see  http://docs.kohanaphp.com/features/localization#time
  * @see  http://php.net/timezones
  */
@@ -12,7 +12,7 @@ date_default_timezone_set('America/Chicago');
 
 /**
  * Enable the Kohana auto-loader.
- * 
+ *
  * @see  http://docs.kohanaphp.com/features/autoloading
  * @see  http://php.net/spl_autoload_register
  */
@@ -20,7 +20,7 @@ spl_autoload_register(array('Kohana', 'auto_load'));
 
 /**
  * Enable Kohana exception handling, adds stack traces and error source.
- * 
+ *
  * @see  http://docs.kohanaphp.com/features/exceptions
  * @see  http://php.net/set_exception_handler
  */
@@ -28,7 +28,7 @@ set_exception_handler(array('Kohana', 'exception_handler'));
 
 /**
  * Enable Kohana error handling, converts all PHP errors to exceptions.
- * 
+ *
  * @see  http://docs.kohanaphp.com/features/exceptions
  * @see  http://php.net/set_error_handler
  */
@@ -38,16 +38,26 @@ set_error_handler(array('Kohana', 'error_handler'));
 
 /**
  * Initialize Kohana, setting the default options.
+ *
+ * The following options are available:
+ * - base_url:   path, and optionally domain, of your application
+ * - index_file: name of your index file, usually "index.php"
+ * - charset:    internal character set used for input and output
+ * - profile:    enable or disable internal profiling
+ * - caching:    enable or disable internal caching
  */
-Kohana::init(array('charset' => 'utf-8', 'base_url' => '/ko3/', 'index_file' => 'index.php'));
+Kohana::init(array('charset' => 'utf-8', 'base_url' => '/ko3/'));
 
 /**
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	// 'orm'      => MODPATH.'orm',
-	'database' => MODPATH.'database',
-	'todoist'  => MODPATH.'todoist',
+	// 'database'   => MODPATH.'database',   // Database access
+	// 'image'      => MODPATH.'image',      // Image manipulation (not complete)
+	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping (not complete)
+	// 'pagination' => MODPATH.'pagination', // Paging of results (not complete)
+	// 'paypal'     => MODPATH.'paypal',     // PayPal integration (not complete)
+	// 'todoist'    => MODPATH.'todoist',    // Todoist integration
 	));
 
 /**
@@ -57,15 +67,9 @@ Kohana::modules(array(
 Kohana::$log->attach(new Kohana_Log_File(APPPATH.'logs'));
 
 /**
- * Set the routes.
+ * Set the routes. Each route must have a minimum of a name, a URI and a set of
+ * defaults for the URI.
  */
-
-Route::set('test', 'test/(<controller>(/<action>))')
-	->defaults(array(
-		'directory'  => 'test',
-		'controller' => 'list',
-		'action'     => 'index'));
-
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'welcome',
