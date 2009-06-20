ca656e0ac5adf526996f952405dd41ebcab8b48e
Woody Gilk <woody.gilk@kohanaphp.com>
2009-06-20 13:32:34 -0500

    Fixing database configuration, fixes #29

--- modules/database/config/database.php
+++ modules/database/config/database.php
@@ -1,49 +1,29 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-/**
- * @package  Database
- *
- * Database connection settings, defined as arrays, or "groups". If no group
- * name is used when loading the database library, the group named "default"
- * will be used.
- *
- * Each group can be connected to independently, and multiple groups can be
- * connected at once.
- *
- * Group Options:
- *  benchmark     - Enable or disable database benchmarking
- *  persistent    - Enable or disable a persistent connection
- *  connection    - Array of connection specific parameters; alternatively,
- *                  you can use a DSN though it is not as fast and certain
- *                  characters could create problems (like an '@' character
- *                  in a password):
- *                  'connection'    => 'mysql://dbuser:secret@localhost/kohana'
- *  character_set - Database character set
- *  table_prefix  - Database table prefix
- *  object        - Enable or disable object results
- *  cache         - Enable or disable query caching
- *	escape        - Enable automatic query builder escaping
- */
-return array(
 
+return array
+(
 	'default' => array
 	(
-		'benchmark'     => TRUE,
-		'persistent'    => FALSE,
-		'connection'    => array
-		(
-			'type'     => 'mysql',
-			'user'     => 'dbuser',
-			'pass'     => 'p@ssw0rd',
-			'host'     => 'localhost',
-			'port'     => FALSE,
-			'socket'   => FALSE,
-			'database' => 'kohana'
+		'type'       => 'mysql',
+		'connection' => array(
+			/**
+			 * The following options can go here:
+			 *
+			 * string   hostname
+			 * integer  port
+			 * string   socket
+			 * string   username
+			 * string   password
+			 * boolean  persistent
+			 */
+			'hostname'   => 'localhost',
+			'username'   => 'root',
+			'password'   => 'r00tdb',
+			'persistent' => FALSE,
 		),
-		'character_set' => 'utf8',
-		'table_prefix'  => '',
-		'object'        => TRUE,
-		'cache'         => FALSE,
-		'escape'        => TRUE
+		'database'   => 'kohana',
+		'charset'    => 'utf8',
+		'caching'    => FALSE,
+		'profiling'  => TRUE,
 	),
-
 );
