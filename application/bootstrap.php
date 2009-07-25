9966063787cef9c47b80240b9ea53269b1110332
Woody Gilk <woody.gilk@kohanaphp.com>
2009-07-25 18:10:52 -0500

    Added "cache_dir" to the Kohana::init() comment

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -37,8 +37,9 @@ spl_autoload_register(array('Kohana', 'auto_load'));
  * - string   base_url    path, and optionally domain, of your application   NULL
  * - string   index_file  name of your index file, usually "index.php"       index.php
  * - string   charset     internal character set used for input and output   utf-8
- * - boolean  profile     enable or disable internal profiling               TRUE
+ * - string   cache_dir   set the internal cache directory                   APPPATH/cache
  * - boolean  errors      enable or disable error handling                   TRUE
+ * - boolean  profile     enable or disable internal profiling               TRUE
  * - boolean  caching     enable or disable internal caching                 FALSE
  */
 Kohana::init(array('base_url' => '/ko3/'));
