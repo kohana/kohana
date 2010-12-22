c694643824b0b43e6c9a668183fdabfe2524ca98
Isaiah DeRose-Wilson <isaiah@iitechs.com>
2010-12-22 12:47:17 -0500

    Use body() setter method, refs #3498

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -4,7 +4,7 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body = 'hello, world!';
+		$this->response->body('hello, world!');
 	}
 
 } // End Welcome
