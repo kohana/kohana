abeccd874f5bdea0392401cd627ec5e937897647
Bob Eagan <bob@synapsestudios.com>
2010-12-30 00:52:24 +0800

    fixing coding standards: incorrect usage of single line statements

--- index.php
+++ index.php
@@ -58,15 +58,21 @@ define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
 // Make the application relative to the docroot
 if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
+{
 	$application = DOCROOT.$application;
+}
 
 // Make the modules relative to the docroot
 if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
+{
 	$modules = DOCROOT.$modules;
+}
 
 // Make the system relative to the docroot
 if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
+{
 	$system = DOCROOT.$system;
+}
 
 // Define the absolute paths for configured directories
 define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
