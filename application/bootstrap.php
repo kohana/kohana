9490410e239ac08ce780d9e2c5acf0f8f51fb96a
Kiall Mac Innes <kiall@managedit.ie>
2011-09-23 17:22:06 +0100

    Fixing CS Issues. Uppercase Constants and Logical Operators

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 // -- Environment setup --------------------------------------------------------
 
--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 class Controller_Welcome extends Controller {
 
