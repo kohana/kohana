eb4c884f6270fda2bffb3d0f8c83de128f176ba4
Kiall Mac Innes <kiall@managedit.ie>
2012-02-16 03:12:15 +0000

    Add minion module to bootstrap.php. Refs #3989.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -110,6 +110,7 @@ Kohana::modules(array(
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
 	// 'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
+	// 'minion'     => MODPATH.'minion',     // CLI Tasks
 	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
