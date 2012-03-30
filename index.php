236574977ee5ba393f3a3d895418041b4f19c33a
Kiall Mac Innes <kiall@managedit.ie>
2012-03-30 18:33:17 +0100

    Correct Minion exception handler class name

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ require APPPATH.'bootstrap'.EXT;
 if (PHP_SAPI == 'cli') // Try and load minion
 {
 	class_exists('Minion_Task') OR die('Please enable the Minion module for CLI support.');
-	set_exception_handler(array('Kohana_Minion_Exception_Handler', 'handler'));
+	set_exception_handler(array('Minion_Exception', 'handler'));
 
 	Minion_Task::factory(Minion_CLI::options())->execute();
 }
