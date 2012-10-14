8bc1386c064ec10a93fe774bffa000c31e801ead
Lorenzo Pisani <zeelot3k@gmail.com>
2012-10-13 19:45:19 -0700

    no longer need to capitalize default controllers and directories

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -124,6 +124,6 @@ Kohana::modules(array(
  */
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'controller' => 'Welcome',
+		'controller' => 'welcome',
 		'action'     => 'index',
 	));
