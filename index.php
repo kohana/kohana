9c4376b10e0632489a3463609e03d6b2a0cbad48
Alex Lenton <lenton64@gmail.com>
2014-06-06 17:22:48 +0100

    Removed repetition in setting paths code

--- index.php
+++ index.php
@@ -15,27 +15,32 @@
 error_reporting(E_ALL | E_STRICT);
 
 /**
- * The directory in which your application specific resources are located.
- * The application directory must contain the bootstrap.php file.
- *
- * @link http://kohanaframework.org/guide/about.install#application
+ * Set paths
  */
-$application = 'application';
+$paths = array(
+	/**
+	 * The directory in which your application specific resources are located.
+	 * The application directory must contain the bootstrap.php file.
+	 *
+	 * @link http://kohanaframework.org/guide/about.install#application
+	 */
+	'APPPATH' => 'application',
 
-/**
- * The directory in which your modules are located.
- *
- * @link http://kohanaframework.org/guide/about.install#modules
- */
-$modules = 'modules';
+	/**
+	 * The directory in which your modules are located.
+	 *
+	 * @link http://kohanaframework.org/guide/about.install#modules
+	 */
+	'MODPATH' => 'modules',
 
-/**
- * The directory in which the Kohana resources are located. The system
- * directory must contain the classes/kohana.php file.
- *
- * @link http://kohanaframework.org/guide/about.install#system
- */
-$system = 'system';
+	/**
+	 * The directory in which the Kohana resources are located. The system
+	 * directory must contain the classes/kohana.php file.
+	 *
+	 * @link http://kohanaframework.org/guide/about.install#system
+	 */
+	'SYSPATH' => 'system',
+);
 
 /**
  * The default extension of resource files. If you change this, all resources
@@ -55,25 +60,21 @@ define('EXT', '.php');
 // Set the full path to the docroot
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
-// Make the application relative to the docroot, for symlink'd index.php
-if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
-	$application = DOCROOT.$application;
-
-// Make the modules relative to the docroot, for symlink'd index.php
-if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
-	$modules = DOCROOT.$modules;
-
-// Make the system relative to the docroot, for symlink'd index.php
-if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
-	$system = DOCROOT.$system;
-
-// Define the absolute paths for configured directories
-define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
-define('MODPATH', realpath($modules).DIRECTORY_SEPARATOR);
-define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
+// For each path set
+foreach ($paths as $key => $path)
+{
+	// Make the path relative to the docroot, for symlink'd index.php
+	if ( ! is_dir($path) AND is_dir(DOCROOT.$path))
+	{
+		$path = DOCROOT.$path;
+	}
+	
+	// Define the absolute path
+	define($key, realpath($path).DIRECTORY_SEPARATOR);
+}
 
 // Clean up the configuration vars
-unset($application, $modules, $system);
+unset($paths);
 
 if (file_exists('install'.EXT))
 {
